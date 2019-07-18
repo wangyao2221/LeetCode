c4a3642d9241fd857065e123730848dc86b8cfcd
wangyao2221 <wangyao2221@163.com>
2019-07-19 00:06:39 +0800

    303. Range Sum Query - Immutable

--- src/RangeSumQueryImmutable/NumArray.java
+++ src/RangeSumQueryImmutable/NumArray.java
@@ -4,28 +4,32 @@ class NumArray {
     private int[][] dp;
 
     public NumArray(int[] nums) {
-        dp = new int[nums.length + 1][nums.length + 1];
+        if (nums.length == 0) return;
 
-        for (int i = 1; i <= nums.length; i++) {
-            dp[0][i] = dp[0][i - 1] + nums[i - 1];
+        dp = new int[nums.length][nums.length];
+
+        dp[0][0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[0][i] = dp[0][i - 1] + nums[i];
         }
 
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = i; j <= nums.length; j++) {
-                dp[i][j] = dp[i - 1][j] - dp[i - 1][0];
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                dp[i][j] = dp[i - 1][j] - nums[i - 1];
             }
         }
 
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp.length; j++) {
-                System.out.print(dp[i][j] + "\t");
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp.length; j++) {
+//                System.out.print(dp[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
     }
     
     public int sumRange(int i, int j) {
-        return dp[i + 1][j + 1];
+        return dp[i][j];
     }
 
     public static void main(String[] args) {
