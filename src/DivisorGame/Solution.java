8f85054861338ac8683ce002c9105b34f59a2a9d
wangyao2221 <wangyao2221@163.com>
2019-07-21 21:03:11 +0800

    1025. Divisor Game

---
+++ src/DivisorGame/Solution.java
@@ -0,0 +1,14 @@
+package DivisorGame;
+
+class Solution {
+    // 10 5 4 3 2
+    // 5 1 1
+    // 1 1
+    public boolean divisorGame(int N) {
+        return N % 2 == 0 ? true: false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().divisorGame(3 ));
+    }
+}
