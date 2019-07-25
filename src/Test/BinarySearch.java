10c599526c4063722399fb12645babbd6acf7ed7
wangyao2221 <wangyao2221@163.com>
2019-07-25 22:39:19 +0800

    Binary Search

---
+++ src/Test/BinarySearch.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class BinarySearch {
+    public int binarySearch(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high) {
+            if (arr[mid] == x) {
+                return mid;
+            } else if (x > arr[mid]){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+            mid = (low + high) / 2;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BinarySearch().binarySearch(new int[]{1,2,3,4}, 4));
+    }
+}
