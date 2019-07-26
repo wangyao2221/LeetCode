539578730f177f6056f3f41eaf5669d5ea4ef64d
wangyao2221 <wangyao2221@163.com>
2019-07-26 23:58:00 +0800

    62. Unique Paths

--- src/UniquePaths/Solution.java
+++ src/UniquePaths/Solution.java
@@ -5,19 +5,28 @@ class Solution {
         if (m == 0 || n == 0) return 0;
         if (m == 1 || n == 1) return 1;
 
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dp = new int[m][n];
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + 2;
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        return dp[m][n];
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
-        int m = 3, n = 2;
+        int m = 7, n = 3;
         System.out.println(new Solution().uniquePaths(m, n));
     }
 }
