3b6c74f74ac2804f1ef05fc1bb550da0d083c712
wangyao2221 <wangyao2221@163.com>
2019-08-08 16:20:51 +0800

    100. Same Tree

---
+++ src/SameTree/Solution.java
@@ -0,0 +1,22 @@
+package SameTree;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null && q != null) return false;
+        if (p != null && q == null) return false;
+        if ((p != null) && (q != null) && (p.val != q.val)) return false;
+
+        return true && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
