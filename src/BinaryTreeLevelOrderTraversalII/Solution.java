04bec01547f5003f8bec6519197b4937c5dd3380
wangyao2221 <wangyao2221@163.com>
2019-08-12 23:59:30 +0800

    107. Binary Tree Level Order Traversal II

--- src/BinaryTreeLevelOrderTraversalII/Solution.java
+++ src/BinaryTreeLevelOrderTraversalII/Solution.java
@@ -1,10 +1,48 @@
 package BinaryTreeLevelOrderTraversalII;
 
-import java.util.List;
+import java.security.Key;
+import java.util.*;
 
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNodeWithDept> queue = new LinkedList<>();
+        Stack<TreeNodeWithDept> stack = new Stack<>();
+
+        queue.add(new TreeNodeWithDept(root, 0));
+        stack.push(new TreeNodeWithDept(root, 0));
+
+        while (!queue.isEmpty()) {
+            TreeNodeWithDept node = queue.poll();
+            TreeNode left = node.node.left;
+            TreeNode right = node.node.right;
+
+            if (left != null) {
+                stack.push(new TreeNodeWithDept(left, node.dept + 1));
+                queue.add(new TreeNodeWithDept(left,node.dept + 1));
+            }
+
+            if (right != null) {
+                stack.push(new TreeNodeWithDept(right, node.dept + 1));
+                queue.add(new TreeNodeWithDept(right,node.dept + 1));
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            TreeNodeWithDept node = stack.pop();
+            
+        }
+
         return null;
+    }
+
+    public class TreeNodeWithDept {
+        TreeNode node;
+        int dept;
+
+        public TreeNodeWithDept(TreeNode node, int dept) {
+            this.node = node;
+            this.dept = dept;
+        }
     }
 
     public class TreeNode {
