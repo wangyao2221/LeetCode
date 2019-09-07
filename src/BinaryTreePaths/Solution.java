4a6eda03b55ccfb59c41b62d22cbb21822c19be5
wangyao2221 <wangyao2221@163.com>
2019-09-08 00:20:00 +0800

    257. Binary Tree Paths

--- src/BinaryTreePaths/Solution.java
+++ src/BinaryTreePaths/Solution.java
@@ -3,10 +3,39 @@ package BinaryTreePaths;
 
 import common.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        return null;
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        List<String> result = new ArrayList<>();
+
+        if (root.left == null && root.right == null) {
+            result.add("" + root.val);
+            return result;
+        }
+
+        List<String> leftList = binaryTreePaths(root.left);
+        List<String> rightList = binaryTreePaths(root.right);
+
+        if (leftList.size() != 0) {
+            for (int i = 0; i < leftList.size(); i++) {
+                leftList.set(i, root.val + "->" + leftList.get(i));
+            }
+            result.addAll(leftList);
+        }
+
+        if (rightList.size() != 0) {
+            for (int i = 0; i < rightList.size(); i++) {
+                rightList.set(i, root.val + "->" + rightList.get(i));
+            }
+            result.addAll(rightList);
+        }
+
+        return result;
     }
 }
