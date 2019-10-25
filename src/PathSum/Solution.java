afcc9e9de0031b18e69b7cbc7c3e16bf264b6957
wangyao2221 <wangyao2221@163.com>
2019-10-25 23:28:48 +0800

    TODO Path Sum

---
+++ src/PathSum/Solution.java
@@ -0,0 +1,16 @@
+package PathSum;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        
+    }
+}
