1b9499d2dc09d8ac350159d71d469d7267c75bb8
wangyao2221 <wangyao2221@163.com>
2019-11-10 12:16:08 +0800

    1254. Number of Closed Islands

---
+++ src/WeeklyContest162/NumberOfClosedIslands/Solution.java
@@ -0,0 +1,10 @@
+package WeeklyContest162.NumberOfClosedIslands;
+
+// TODO
+class Solution {
+    public int closedIsland(int[][] grid) {
+        int result = 0;
+
+        return result;
+    }
+}
