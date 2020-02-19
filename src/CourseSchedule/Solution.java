88c5c567f87d073e70370b7513ac8035e90db7fe
wangyao2221 <wangyao2221@163.com>
2020-02-20 00:12:07 +0800

    207. Course Schedule : 利用拓扑排序的思想解题

--- src/CourseSchedule/Solution.java
+++ src/CourseSchedule/Solution.java
@@ -1,29 +1,50 @@
 package CourseSchedule;
 
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public static class Graph {
-        int val;
-        List<Integer> nextList;
-
-        public Graph() {
-        }
-
-        public Graph(int val) {
-            this.val = val;
-        }
-
-        public Graph(int val, List<Integer> nextList) {
-            this.val = val;
-            this.nextList = nextList;
-        }
-    }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] finished = new int[numCourses];
+        HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        int finished = 0;
         int[] inDegrees = new int[numCourses];
 
-        return false;
+        // 用map构造有向图
+        for (int i = 0; i < prerequisites.length; i++) {
+            int in = prerequisites[i][0];
+            int out = prerequisites[i][1];
+            if (!map.containsKey(in)) {
+                map.put(in, new ArrayList<Integer>());
+            }
+            map.get(in).add(out);
+            // 计算入度
+            inDegrees[out]++;
+        }
+
+        // 找出第一轮入度为1的课程
+        Queue<Integer> zeroInQueue = new LinkedList<Integer>();
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                zeroInQueue.offer(i);
+            }
+        }
+
+        // 用拓扑排序的方法计算可完成的课程
+        while (!zeroInQueue.isEmpty()) {
+            int course = zeroInQueue.poll();
+            finished++;
+            List<Integer> nextCourses = map.get(course);
+            if (nextCourses != null) {
+                for (Integer nextCourse : nextCourses) {
+                    if (inDegrees[nextCourse] != 0) {
+                        inDegrees[nextCourse]--;
+                        if (inDegrees[nextCourse] == 0) {
+                            zeroInQueue.offer(nextCourse);
+                        }
+                    }
+                }
+            }
+        }
+
+        return finished == numCourses;
     }
 }
