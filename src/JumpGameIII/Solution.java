1222c7c7a8f8c2a7d8b0d77c8c61ce55bcdd1368
wangyao2221 <wangyao2221@163.com>
2020-02-21 23:56:35 +0800

    1043. Partition Array for Maximum Sum

---
+++ src/JumpGameIII/Solution.java
@@ -0,0 +1,12 @@
+package JumpGameIII;
+
+/**
+ * 思路：
+ * 1、首先构建一个下标到下标之间的可达性的图
+ * 2、从start出发DFS搜索是否有可达0所在位置的路径
+ */
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        return false;
+    }
+}
