514a92e8bb81ac75caa67b28ad2709bbd1f6cfba
wangyao2221 <wangyao2221@163.com>
2020-02-21 22:12:47 +0800

    1043. Partition Array for Maximum Sum

--- src/PartitionArrayForMaximumSum/Solution.java
+++ src/PartitionArrayForMaximumSum/Solution.java
@@ -2,6 +2,15 @@ package PartitionArrayForMaximumSum;
 
 class Solution {
     public int maxSumAfterPartitioning(int[] A, int K) {
-        return 0;
+        int[] dp = new int[A.length + 1];
+        dp[0] = 0;
+        for (int i = 0; i < A.length; i++) {
+            int tmpMax = A[i];
+            for (int j = i; j > i - K && j >= 0; j--) {
+                tmpMax = Math.max(tmpMax, A[j]);
+                dp[i + 1] = Math.max(dp[i + 1], dp[j] + tmpMax * (i - j + 1));
+            }
+        }
+        return dp[A.length];
     }
 }
