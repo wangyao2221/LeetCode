c04c27fdd058ac86326c63a31bc0fcd3f01b81c0
wangyao2221 <wangyao2221@163.com>
2020-02-22 00:57:13 +0800

    1306. Jump Game III

--- src/JumpGameIII/Solution.java
+++ src/JumpGameIII/Solution.java
@@ -7,6 +7,24 @@ package JumpGameIII;
  */
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        return false;
+//        int[][] graph = new int[arr.length][2]; 这一行初始化竟会导致运行时间增加8ms
+        boolean[] visit = new boolean[arr.length];
+
+        return dfs(arr, start, visit);
+    }
+
+    public boolean dfs(int[] arr, int start, boolean[] visit) {
+        if (start < 0 || start >= arr.length || visit[start]) {
+            return false;
+        }
+        if (arr[start] == 0) {
+            return true;
+        }
+
+        int jump = arr[start];
+        visit[start] = true;
+        //        visit[start] = false; 不需要，这条路径经过了该点，也就表明经过该点的路径都不会到到值为0的位置
+
+        return dfs(arr, start - jump,  visit) || dfs(arr, start + jump, visit);
     }
 }
