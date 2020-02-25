1eed469f52c262f21f58f3eed322b399925da93a
wangyao2221 <wangyao2221@163.com>
2020-02-25 22:34:12 +0800

    202. Happy Number

---
+++ src/HappyNumber/Solution.java
@@ -0,0 +1,23 @@
+package HappyNumber;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(n);
+        
+        while(true) {
+            int tmpN = n;
+            n = 0;
+            while(tmpN != 0) {
+                int remainder = tmpN % 10;
+                n += remainder * remainder;
+                tmpN /= 10;
+            }
+            if(n == 1) return true;
+            if(!set.add(n)) return false;
+        }
+    }
+}
