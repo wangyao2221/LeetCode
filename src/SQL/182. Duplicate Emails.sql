66795786391a674bc539b73848df246c2601d91b
wangyao2221 <wangyao2221@163.com>
2020-03-01 00:28:11 +0800

    182. Duplicate Emails

---
+++ src/SQL/182. Duplicate Emails.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (Id, Email) values ('1', 'a@b.com');
+insert into Person (Id, Email) values ('2', 'c@d.com');
+insert into Person (Id, Email) values ('3', 'a@b.com');
+
+select Email from Person group by Email having count(Email) > 1
