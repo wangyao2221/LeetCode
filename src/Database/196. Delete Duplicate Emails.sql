f911503db3fecc50a4146da03a20b8778de6a7c7
wangyao2221 <wangyao2221@163.com>
2020-03-06 23:51:49 +0800

    196. Delete Duplicate Emails

--- src/Database/196. Delete Duplicate Emails.sql
+++ src/Database/196. Delete Duplicate Emails.sql
@@ -1,4 +1,10 @@
-create table Person196 (Id int primary key , Email varchar(255));
-create view view_person as select distinct * from Person;
-truncate table person;
-insert into Person select * from view_person;
+# create table Person196 (Id int primary key , Email varchar(255));
+# insert into Person196 values (1 , 'john@example.com'), (2, 'bob@example.com'), (3, 'john@example.com');
+# drop table if exists person_tmp;
+# create table if not exists person_tmp like person196;
+# truncate table person_tmp;
+# insert into person_tmp select Id, Email from person196 group by Email;
+# truncate table person196;
+# insert into person196 select * from person_tmp;
+
+delete p1 from Person196 p1, Person196 p2 where p1.Id > p2.Id and p1.Email = p2.Email
