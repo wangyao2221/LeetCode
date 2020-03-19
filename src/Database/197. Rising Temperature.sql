876a4a8ed312a51e74f48d9249ee9c39ac06b436
wangyao2221 <wangyao2221@163.com>
2020-03-19 23:47:34 +0800

    197. Rising Temperature

---
+++ src/Database/197. Rising Temperature.sql
@@ -0,0 +1,10 @@
+Create table If Not Exists Weather (Id int, RecordDate date, Temperature int);
+Truncate table Weather;
+insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10');
+insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25');
+insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20');
+insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30');
+
+select a.Id from Weather a join Weather b
+    on DATE_SUB(a.RecordDate, interval 1 day) = b.RecordDate
+where a.Temperature > b.Temperature;
