db30004ae247c00d9ed1eb70dec874cd28b5134e
wangyao2221 <wangyao2221@163.com>
2020-03-25 23:27:59 +0800

    剑指offer 树定义

---
+++ src/ForOffer/common/TreeNode.java
@@ -0,0 +1,11 @@
+package ForOffer.common;
+
+public class TreeNode {
+    int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
